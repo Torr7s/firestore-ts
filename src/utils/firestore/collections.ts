a8a2c54664490e7e5a99fd2dd0be472e69e6a200
Matheus Torres <torresmiranda.rj@gmail.com>
2022-10-02 12:54:34 -0300

    feat: delete files due to repository usage

--- src/utils/firestore/collections.ts
+++
@@ -1,6 +0,0 @@
-import { CollectionReference } from 'firebase-admin/firestore';
-
-import { createCollection } from './helpers/create-collection';
-import { User } from 'src/types/user.type';
-
-export const usersCollection: CollectionReference<User> = createCollection<User>('users');
