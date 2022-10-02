0d99ae211eec8f8833189c9c0536860cafb877b9
Matheus Torres <torresmiranda.rj@gmail.com>
2022-10-02 13:01:38 -0300

    feat(repositories): create base repository interface

---
+++ src/repositories/base.repository.ts
@@ -0,0 +1,17 @@
+import { WhereFilterOp } from 'firebase-admin/firestore';
+
+import { UserModel } from '../firestore/models/user.model';
+
+export type WithDocId<T> = { docId: string } & T;
+
+export interface BaseRepository<T> {
+  create(data: T): Promise<void>;
+  find(path: string, filter: WhereFilterOp, value: any): Promise<WithDocId<T>>;
+  list(): Promise<WithDocId<T>[]>;
+  update(userId: string, data: Partial<T>): Promise<void>;
+}
+
+export interface UsersRepository extends BaseRepository<UserModel> {
+  findByEmail(email: string): Promise<WithDocId<UserModel>>;
+  findById(userId: string): Promise<WithDocId<UserModel>>;
+}
