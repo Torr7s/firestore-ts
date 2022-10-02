82784c468cbdf59def69717cdb63306553254e87
Matheus Torres <torresmiranda.rj@gmail.com>
2022-10-02 18:16:44 -0300

    feat(repositories): path will now receive keys from the generic
    will allow more control over which property to use

--- src/repositories/base.repository.ts
+++ src/repositories/base.repository.ts
@@ -6,7 +6,7 @@ export type WithDocId<T> = { docId: string } & T;
 
 export interface BaseRepository<T> {
   create(data: T): Promise<void>;
-  find(path: string, filter: WhereFilterOp, value: any): Promise<WithDocId<T>>;
+  find(path: keyof T, filter: WhereFilterOp, value: any): Promise<WithDocId<T>>;
   list(): Promise<WithDocId<T>[]>;
   update(userId: string, data: Partial<T>): Promise<void>;
 }
